a79ae0ac4f38c8599069b0e63613c7a04a10707f
NinjaFarris <nathanif@stud.ntnu.no>
2024-11-03 14:35:17 +0100

    Important lesson: Clock has to be configured properly

--- MPLABProject/ledflashing.X/main.c
+++ MPLABProject/ledflashing.X/main.c
@@ -1,17 +1,138 @@
 #include <sam.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdlib.h> 
+#define CPU_CLOCK_FREQUENCY 120000000
 
-// Delay function for a simple blocking delay
-void delay_ms(uint32_t ms) {
-    for (volatile uint32_t i = 0; i < (ms * 3000); i++) {
-        __asm("NOP"); // "No Operation"
+#pragma config BOD33_DIS = SET
+#pragma config BOD33USERLEVEL = 0x1cU
+#pragma config BOD33_ACTION = RESET
+#pragma config BOD33_HYST = 0x2U
+#pragma config NVMCTRL_BOOTPROT = 0
+#pragma config NVMCTRL_SEESBLK = 0x0U
+#pragma config NVMCTRL_SEEPSZ = 0x0U
+#pragma config RAMECC_ECCDIS = SET
+#pragma config WDT_ENABLE = CLEAR
+#pragma config WDT_ALWAYSON = CLEAR
+#pragma config WDT_PER = CYC8192
+#pragma config WDT_WINDOW = CYC8192
+#pragma config WDT_EWOFFSET = CYC8192
+#pragma config WDT_WEN = CLEAR
+#pragma config NVMCTRL_REGION_LOCKS = 0xffffffffU
+
+
+
+static void OSCCTRL_Initialize(void)
+{
+
+}
+
+static void OSC32KCTRL_Initialize(void)
+{
+
+    OSC32KCTRL_REGS->OSC32KCTRL_RTCCTRL = OSC32KCTRL_RTCCTRL_RTCSEL(0U);
+}
+
+static void FDPLL0_Initialize(void)
+{
+    GCLK_REGS->GCLK_PCHCTRL[1] = GCLK_PCHCTRL_GEN(0x2U)  | GCLK_PCHCTRL_CHEN_Msk;
+    while ((GCLK_REGS->GCLK_PCHCTRL[1] & GCLK_PCHCTRL_CHEN_Msk) != GCLK_PCHCTRL_CHEN_Msk)
+    {
+        /* Wait for synchronization */
+    }
+
+    /****************** DPLL0 Initialization  *********************************/
+
+    /* Configure DPLL    */
+    OSCCTRL_REGS->DPLL[0].OSCCTRL_DPLLCTRLB = OSCCTRL_DPLLCTRLB_FILTER(0U) | OSCCTRL_DPLLCTRLB_LTIME(0x0U)| OSCCTRL_DPLLCTRLB_REFCLK(0U) ;
+
+
+    OSCCTRL_REGS->DPLL[0].OSCCTRL_DPLLRATIO = OSCCTRL_DPLLRATIO_LDRFRAC(0U) | OSCCTRL_DPLLRATIO_LDR(119U);
+
+    while((OSCCTRL_REGS->DPLL[0].OSCCTRL_DPLLSYNCBUSY & OSCCTRL_DPLLSYNCBUSY_DPLLRATIO_Msk) == OSCCTRL_DPLLSYNCBUSY_DPLLRATIO_Msk)
+    {
+        /* Waiting for the synchronization */
+    }
+
+    /* Enable DPLL */
+    OSCCTRL_REGS->DPLL[0].OSCCTRL_DPLLCTRLA = OSCCTRL_DPLLCTRLA_ENABLE_Msk   ;
+
+    while((OSCCTRL_REGS->DPLL[0].OSCCTRL_DPLLSYNCBUSY & OSCCTRL_DPLLSYNCBUSY_ENABLE_Msk) == OSCCTRL_DPLLSYNCBUSY_ENABLE_Msk )
+    {
+        /* Waiting for the DPLL enable synchronization */
+    }
+
+    while((OSCCTRL_REGS->DPLL[0].OSCCTRL_DPLLSTATUS & (OSCCTRL_DPLLSTATUS_LOCK_Msk | OSCCTRL_DPLLSTATUS_CLKRDY_Msk)) !=
+                (OSCCTRL_DPLLSTATUS_LOCK_Msk | OSCCTRL_DPLLSTATUS_CLKRDY_Msk))
+    {
+        /* Waiting for the Ready state */
     }
 }
 
-// Configure button and LED pins
-void configure_pins(void) {
-    // Enable the clock for Port B and C
+
+static void DFLL_Initialize(void)
+{
+}
+
+
+static void GCLK0_Initialize(void)
+{
+
+    /* selection of the CPU clock Division */
+    MCLK_REGS->MCLK_CPUDIV = MCLK_CPUDIV_DIV(0x01U);
+
+    while((MCLK_REGS->MCLK_INTFLAG & MCLK_INTFLAG_CKRDY_Msk) != MCLK_INTFLAG_CKRDY_Msk)
+    {
+        /* Wait for the Main Clock to be Ready */
+    }
+    GCLK_REGS->GCLK_GENCTRL[0] = GCLK_GENCTRL_DIV(1U) | GCLK_GENCTRL_SRC(7U) | GCLK_GENCTRL_GENEN_Msk;
+
+    while((GCLK_REGS->GCLK_SYNCBUSY & GCLK_SYNCBUSY_GENCTRL_GCLK0) == GCLK_SYNCBUSY_GENCTRL_GCLK0)
+    {
+        /* wait for the Generator 0 synchronization */
+    }
+}
+
+static void GCLK1_Initialize(void)
+{
+    GCLK_REGS->GCLK_GENCTRL[1] = GCLK_GENCTRL_DIV(2U) | GCLK_GENCTRL_SRC(7U) | GCLK_GENCTRL_GENEN_Msk;
+
+    while((GCLK_REGS->GCLK_SYNCBUSY & GCLK_SYNCBUSY_GENCTRL_GCLK1) == GCLK_SYNCBUSY_GENCTRL_GCLK1)
+    {
+        /* wait for the Generator 1 synchronization */
+    }
+}
+
+static void GCLK2_Initialize(void)
+{
+    GCLK_REGS->GCLK_GENCTRL[2] = GCLK_GENCTRL_DIV(48U) | GCLK_GENCTRL_SRC(6U) | GCLK_GENCTRL_GENEN_Msk;
+
+    while((GCLK_REGS->GCLK_SYNCBUSY & GCLK_SYNCBUSY_GENCTRL_GCLK2) == GCLK_SYNCBUSY_GENCTRL_GCLK2)
+    {
+        /* wait for the Generator 2 synchronization */
+    }
+}
+
+void CLOCK_Initialize (void)
+{
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID - H3_MISRAC_2012_R_2_2_DR_2 */
+
+    /* Function to Initialize the Oscillators */
+    OSCCTRL_Initialize();
+
+    /* Function to Initialize the 32KHz Oscillators */
+    OSC32KCTRL_Initialize();
+
+    DFLL_Initialize();
+    GCLK2_Initialize();
+    FDPLL0_Initialize();
+    GCLK0_Initialize();
+    GCLK1_Initialize();
+
+    /* MISRAC 2012 deviation block end */
+
+    /* Selection of the Generator and write Lock for EIC */
     GCLK_REGS->GCLK_PCHCTRL[4] = GCLK_PCHCTRL_GEN(0x1U)  | GCLK_PCHCTRL_CHEN_Msk;
 
     while ((GCLK_REGS->GCLK_PCHCTRL[4] & GCLK_PCHCTRL_CHEN_Msk) != GCLK_PCHCTRL_CHEN_Msk)
@@ -19,9 +140,20 @@ void configure_pins(void) {
         /* Wait for synchronization */
     }
 
-    
-    MCLK_REGS->MCLK_APBBMASK |= MCLK_APBBMASK_PORT_Msk;
-    MCLK_REGS->MCLK_APBAMASK |= MCLK_APBAMASK_EIC_Msk;
+    /* Configure the AHB Bridge Clocks */
+    MCLK_REGS->MCLK_AHBMASK = 0xffffffU;
+
+    /* Configure the APBA Bridge Clocks */
+    MCLK_REGS->MCLK_APBAMASK = 0x7ffU;
+
+
+}
+
+
+// Configure button and LED pins
+void configure_pins(void) {
+    // Enable the clock for Port B and C
+
 
    PORT_REGS->GROUP[1].PORT_OUT = 0x80080000U;
    PORT_REGS->GROUP[1].PORT_PINCFG[31] = 0x7U;
@@ -87,7 +219,8 @@ void configure_interrupts(void) {
 
 
 int main(void) {
-    configure_pins();       // Configure LED and button pins
+    configure_pins(); 
+    CLOCK_Initialize(); // Configure LED and button pins
     configure_interrupts(); // Set up interrupts for button press
     
 
@@ -98,13 +231,10 @@ int main(void) {
 }
 
 // External Interrupt Handler for EXTINT15
-void __attribute__((used)) EIC_EXTINT_15_InterruptHandler(void){
+void __attribute__((used)) EIC_EXTINT_15_Handler(void){
 
-    
-    if (EIC_REGS->EIC_INTFLAG & (1 << 15)) {  // Check if EXTINT15 triggered the interrupt
-        toggle_led();                         // Toggle the LED
-    }
-    
-    EIC_REGS->EIC_INTFLAG = (1 << 15);    // Clear the interrupt flag for EXTINT15
+    EIC_REGS->EIC_INTFLAG = (1 << 15); 
+    toggle_led();
+       // Clear the interrupt flag for EXTINT15
     return;
 }
