1849ef62659e39248dae8cd47be6c820ff2b2808
heikkkk <henrik.teksle.sandok@hotmail.no>
2025-02-06 21:07:55 +0100

    feat: add clock config and initialization

---
+++ src/motherboad_mcu.X/clock/clock.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   clock.h
+ * Author: heik
+ *
+ * Created on February 6, 2025, 8:56 PM
+ */
+
+#ifndef CLOCK_H
+#define	CLOCK_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+void CLOCK_Initialize(void);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CLOCK_H */
+
