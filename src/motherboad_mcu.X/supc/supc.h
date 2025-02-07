32c2e9a737d03fce954dd793e336f081b683da1c
heikkkk <henrik.teksle.sandok@hotmail.no>
2025-02-07 08:47:34 +0100

    feat: add interrupt handler for BOD33

---
+++ src/motherboad_mcu.X/supc/supc.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   supc.h
+ * Author: heik
+ *
+ * Created on February 7, 2025, 8:11 AM
+ */
+
+#ifndef SUPC_H
+#define	SUPC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* SUPC_H */
+
