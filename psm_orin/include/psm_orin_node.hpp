2072ef3eea3f0fbe0800d7127c6ba17b63562ccc
NinjaFarris <nathanif@stud.ntnu.no>
2025-03-27 17:42:20 +0100

    fix: formatting

--- psm_orin/include/psm_orin_node.hpp
+++ psm_orin/include/psm_orin_node.hpp
@@ -2,26 +2,26 @@
 #define PSM_ORIN_NODE_HPP
 
 #include <chrono>
-#include <std_msgs/msg/float64.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/float64.hpp>
 
 class PSMOrinNode : public rclcpp::Node {
-public:
-  explicit PSMOrinNode();
+   public:
+    explicit PSMOrinNode();
 
-private:
-  void set_subscribers_and_publishers();
-  void publish_voltage();
-  void publish_current();
-  void read_ads_callback();
+   private:
+    void set_subscribers_and_publishers();
+    void publish_voltage();
+    void publish_current();
+    void read_ads_callback();
 
-  rclcpp::TimerBase::SharedPtr watchdog_timer_;
-  rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr voltage_pub_;
-  rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr current_pub_;
-  rclcpp::Time last_msg_time_;
+    rclcpp::TimerBase::SharedPtr watchdog_timer_;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr voltage_pub_;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr current_pub_;
+    rclcpp::Time last_msg_time_;
 
-  double voltage = 0.0;
-  double current = 0.0;
+    double voltage = 0.0;
+    double current = 0.0;
 };
 
 #endif  // PSM_ORIN_NODE_HPP
--- psm_orin/src/psm_orin_node.cpp
+++ psm_orin/src/psm_orin_node.cpp
@@ -3,38 +3,39 @@
 #include "psm_orin_driver.h"
 
 PSMOrinNode::PSMOrinNode() : Node("psm_orin_node") {
-  set_subscribers_and_publishers();
-  i2c_psm_init();
-  config_ads();
+    set_subscribers_and_publishers();
+    i2c_psm_init();
+    config_ads();
 
-  watchdog_timer_ = this->create_wall_timer(
-    std::chrono::milliseconds(500),
-    std::bind(&PSMOrinNode::read_ads_callback, this)
-  );
+    watchdog_timer_ = this->create_wall_timer(
+        std::chrono::milliseconds(500),
+        std::bind(&PSMOrinNode::read_ads_callback, this));
 
-  last_msg_time_ = this->now();
+    last_msg_time_ = this->now();
 }
 
 void PSMOrinNode::set_subscribers_and_publishers() {
-  voltage_pub_ = this->create_publisher<std_msgs::msg::Float64>("voltage", 10);
-  current_pub_ = this->create_publisher<std_msgs::msg::Float64>("current", 10);
+    voltage_pub_ =
+        this->create_publisher<std_msgs::msg::Float64>("voltage", 10);
+    current_pub_ =
+        this->create_publisher<std_msgs::msg::Float64>("current", 10);
 }
 
 void PSMOrinNode::publish_voltage() {
-  auto voltage_msg = std_msgs::msg::Float64();
-  voltage_msg.data = voltage;
-  voltage_pub_->publish(voltage_msg);
+    auto voltage_msg = std_msgs::msg::Float64();
+    voltage_msg.data = voltage;
+    voltage_pub_->publish(voltage_msg);
 }
 
 void PSMOrinNode::publish_current() {
-  auto current_msg = std_msgs::msg::Float64();
-  current_msg.data = current;
-  current_pub_->publish(current_msg);
+    auto current_msg = std_msgs::msg::Float64();
+    current_msg.data = current;
+    current_pub_->publish(current_msg);
 }
 
 void PSMOrinNode::read_ads_callback() {
-  read_scaled_measurements(&voltage, &current);
+    read_scaled_measurements(&voltage, &current);
 
-  publish_current();
-  publish_voltage();
+    publish_current();
+    publish_voltage();
 }
