251d340e3afb6ff88dbcf1f0fcf02a952dffc4c4
jonathan <jonathhu@stud.ntnu.no>
2025-10-09 16:15:19 +0200

    feat: create table of thrusters' tcc channels

--- thruster_mcu/src/main.c
+++ thruster_mcu/src/main.c
@@ -1,59 +1,46 @@
-/*******************************************************************************
-  Main Source File
-
-  Company:
-    Microchip Technology Inc.
-
-  File Name:
-    main.c
-
-  Summary:
-    This file contains the "main" function for a project.
-
-  Description:
-    This file contains the "main" function for a project.  The
-    "main" function calls the "SYS_Initialize" function to initialize the state
-    machines of all modules in the system
- *******************************************************************************/
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Included Files
-// *****************************************************************************
-// *****************************************************************************
-
 #include <stddef.h>                     // Defines NULL
 #include <stdbool.h>                    // Defines true
 #include <stdlib.h>                     // Defines EXIT_FAILURE
 #include "definitions.h"                // SYS function prototypes
 
+const uint32_t tcc0_period = 74000U;
+const uint32_t tcc1_period = 74000U;
+const uint32_t tcc2_period = 18500U;
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: Main Entry Point
-// *****************************************************************************
-// *****************************************************************************
+
+struct Thruster {
+    uint8_t tcc_num;
+    uint8_t channel;
+    uint32_t period;
+};
+
+static const struct Thruster thrusters[8] = {
+    {0, 0, tcc0_period}, // TCC0_CHANNEL0
+    {0, 1, tcc0_period}, // TCC0_CHANNEL1
+    {0, 2, tcc0_period}, // TCC0_CHANNEL2
+    {0, 3, tcc0_period}, // TCC0_CHANNEL3
+    {0, 4, tcc0_period}, // TCC0_CHANNEL4
+    {0, 5, tcc0_period}, // TCC0_CHANNEL5
+    {1, 0, tcc1_period}, // TCC1_CHANNEL0
+    {1, 1, tcc1_period}  // TCC1_CHANNEL1 
+};
+
+
 
 int main ( void )
 {
+    printf(thrusters[0].tcc_num);
     /* Initialize all modules */
     SYS_Initialize ( NULL );
 
     while ( true )
     {
-        //LED0_Clear();
-
-
-        LED0_Clear();
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
     }
 
     /* Execution should not come here during normal operation */
 
     return ( EXIT_FAILURE );
 }
-
-
-/*******************************************************************************
- End of File
-*/
 
