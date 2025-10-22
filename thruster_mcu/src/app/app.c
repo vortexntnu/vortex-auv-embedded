5d4fdc87c97a89d3b677b8fb9a4d83cc98329a1d
jonathan <jonathhu@stud.ntnu.no>
2025-10-22 11:42:59 +0200

    feat: set light pwm period to 20000ms

--- thruster_mcu/src/app/app.c
+++ thruster_mcu/src/app/app.c
@@ -9,6 +9,7 @@ static const uint32_t TCC0_PERIOD                       = 74000U;
 static const uint32_t TCC1_PERIOD                       = 74000U;
 static const uint32_t TCC2_PERIOD                       = 18500U;
 static const uint32_t THRUSTER_PWM_PERIOD_MICROSECONDS  = 20000U;
+static const uint32_t LIGHT_PWM_PERIOD_MICROSECONDS     = 20000U;
 static const uint32_t CAN_EVENT_ID_BASE                 = 0x369U;
 static const uint8_t  MESSAGES_TO_READ                  = 1U;
 
