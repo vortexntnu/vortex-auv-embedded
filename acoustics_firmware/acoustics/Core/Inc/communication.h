763b1fe76a62f917897e51cf464db2f19074b3ea
forrisdahl <nathanif@stud.ntnu.no>
2025-10-26 11:52:53 +0100

    feat: add send_can and send_usart function
    Probably need to add some sort of header to distinguish between different messages

---
+++ acoustics_firmware/acoustics/Core/Inc/communication.h
@@ -0,0 +1,18 @@
+#ifndef COMMUNICATION_H
+#define COMMUNICATION_H
+
+#include <stddef.h>
+#include "stm32h7xx_hal.h"
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+void send_data_can(const void* data, size_t len, FDCAN_HandleTypeDef* can);
+void send_data_usart(const void* data, size_t len, UART_HandleTypeDef* usart);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
