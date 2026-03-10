f8a5a7dacb09459e81e6e7c878d57a118c7f9181
jonathan <jonathhu@stud.ntnu.no>
2026-03-10 14:31:20 +0100

    feat: enable callbacks for killswitch and flt pins

--- thruster_mcu_new/src/app/app.c
+++ thruster_mcu_new/src/app/app.c
@@ -170,10 +170,10 @@ void app_init(void) {
     CAN1_RxFifoCallbackRegister(CAN_RX_FIFO_0, can_receive_callback, (uintptr_t)NULL);
     CAN1_TxFifoCallbackRegister(can_transmit_callback, (uintptr_t)NULL);
     
-    // Configure external interrupts 
-    EIC_CallbackRegister(EIC_PIN_0, eic_pin_killswitch, 0);
+    // Configure callback for killswitch
+    EIC_NMICallbackRegister(eic_pin_killswitch, 0);
     
-    /*
+    // Configure callbacks for FLT pins
     EIC_CallbackRegister(EIC_PIN_0, eic_pin_flt_thruster, 0);
     EIC_CallbackRegister(EIC_PIN_1, eic_pin_flt_thruster, 1);
     EIC_CallbackRegister(EIC_PIN_2, eic_pin_flt_thruster, 2);
@@ -182,10 +182,6 @@ void app_init(void) {
     EIC_CallbackRegister(EIC_PIN_5, eic_pin_flt_thruster, 5);
     EIC_CallbackRegister(EIC_PIN_6, eic_pin_flt_thruster, 6);
     EIC_CallbackRegister(EIC_PIN_7, eic_pin_flt_thruster, 7); 
-     */
-    
-    
-    
     
     // Enable ADC
     ADC0_Enable();
