aac89b6ac06db207b77ddde6918a2cae7d183f26
jonathan <jonathhu@stud.ntnu.no>
2026-03-17 10:38:08 +0100

    docs: remove unneccessary comment

--- thruster_mcu_new/src/app/app.c
+++ thruster_mcu_new/src/app/app.c
@@ -84,7 +84,7 @@ static const struct {
     { 9, 8 },   /* slot 7: AIN9  ? Thruster 8 */
 };
 
-static struct pwm_output lights[1] = {{MPWM_TC, 3, 1, TC3_PERIOD, 1100, 1900, 1100, LIGHT_PWM_PERIOD_US, 1100}}; // TC3_CC1. For MPWM TOP = CC0 and duty cycle is determined by CC1
+static struct pwm_output lights[1] = {{MPWM_TC, 3, 1, TC3_PERIOD, 1100, 1900, 1100, LIGHT_PWM_PERIOD_US, 1100}}; // TC3_CC1
 
 // FOR TESTING
 void generate_pwm_signals();
