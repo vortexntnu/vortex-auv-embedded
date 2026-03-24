f455777400991ca63c76725de3bd39d8333fafb5
jonathan <jonathhu@stud.ntnu.no>
2026-03-24 13:14:10 +0100

    test

--- thruster_mcu_new/src/main.c
+++ thruster_mcu_new/src/main.c
@@ -17,12 +17,12 @@ int main ( void ) {
     
     //test_can_tx();
     
-    uint8_t to_test[] = {0, 1}; // Indices
+    uint8_t to_test[] = {1}; // Indices
     uint8_t count = sizeof(to_test) / sizeof(to_test[0]);
-    uint16_t max_us = 1800;
-    uint16_t min_us = 1200;
+    uint16_t max_us = 1750;
+    uint16_t min_us = 1250;
     uint16_t step_us = 50;
-    uint32_t step_delay_ms = 20;
+    uint32_t step_delay_ms = 10;
     
     test_thrusters(to_test, count, max_us, min_us, step_us, step_delay_ms);
     
