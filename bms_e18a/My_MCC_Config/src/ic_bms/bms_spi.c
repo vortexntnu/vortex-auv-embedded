d954260924951b5baf2d6834e1a132cd85a8e518
forrisdahl <nathanif@stud.ntnu.no>
2026-03-30 17:01:55 +0200

    fix: remove SCDLatch limit and enable recovery for short circuit and over current

--- bms_e18a/My_MCC_Config/src/ic_bms/bms_spi.c
+++ bms_e18a/My_MCC_Config/src/ic_bms/bms_spi.c
@@ -676,6 +676,8 @@ void BQ769x2_Init() {
     //TESTING INIT FUNCTIONS
     BQ769x2_SetRegister(MfgStatusInit, 0x0050, 2);
 
+    BQ769x2_SetRegister(ProtectionConfiguration, 0x0602, 2);
+
     BQ769x2_SetRegister(FETOptions, 0x0D, 1);
     BQ769x2_SetRegister(ChgPumpControl,0x01,1); 
     BQ769x2_SetRegister(CFETOFFPinConfig, 0x00, 1);
@@ -740,12 +742,15 @@ void BQ769x2_Init() {
 	// Set up SCD Threshold - 0x9286 = 0x05 (100 mV = 100A across 1mOhm sense resistor)  0x05=100mV
 	BQ769x2_SetRegister(SCDThreshold, 0x05, 1);
 
+    BQ769x2_SetRegister(OCDLRecoveryThreshold, 0, 2);
+    BQ769x2_SetRegister(SCDLRecoveryThreshold, 0, 2);
+
 	// Set up SCD Delay - 0x9287 = 0x03 (30 us) Enabled with a delay of (value - 1) * 15 µs; min value of 1    
 	BQ769x2_SetRegister(SCDDelay, 0x03, 1);
 
 	// Set up SCDL Latch Limit to 1 to set SCD recovery only with load removal 0x9295 = 0x01
 	// If this is not set, then SCD will recover based on time (SCD Recovery Time parameter).
-	BQ769x2_SetRegister(SCDLLatchLimit, 0x01, 1);
+	BQ769x2_SetRegister(SCDLLatchLimit, 0x00, 1);
 
 	// Exit CONFIGUPDATE mode  - Subcommand 0x0092
 	CommandSubcommands(EXIT_CFGUPDATE);
